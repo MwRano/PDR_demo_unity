aad69c7624effe7f36a98274b7d92c4eecbfc4a2
VICTUS_RANO\MWAM1 <hitoki0327@icloud.com>
2025-05-05 23:24:27 +0900

    add restart

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        Button restartButton = gameObject.GetComponent<Button>();
+        
+        if (restartButton != null)
+        {
+            restartButton.onClick.AddListener(OnRestartButtonClicked); // ボタンがクリックされたときにOnRestartButtonClickedメソッドを呼び出す
+        }
+        else
+        {
+            Debug.LogError("Button component not found on the GameObject.");
+        }
+    }
+
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
