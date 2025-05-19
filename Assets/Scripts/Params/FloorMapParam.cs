d357c3bab382ad1bce5e246e437610c69301c0e0
MwRano <noratohira@gmail.com>
2025-05-19 16:05:10 +0900

    fix FloorMapParam

--- Assets/Scripts/Params/FloorMapParam.cs
+++ Assets/Scripts/Params/FloorMapParam.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FloorMapParam", menuName = "Scriptable Objects/FloorMapParam")]
 public class FloorMapParam : ScriptableObject
 {
-    [SerializeField] List<FloorMapData> floorMapDataList;
+    public List<FloorMapData> floorMapDataList;
 }
