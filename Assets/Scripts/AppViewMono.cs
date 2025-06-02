6ea131b8a60fb65122160170722acd3c4ff6e833
MwRano <noratohira@gmail.com>
2025-06-02 20:33:51 +0900

    Add AppViewMono

--- Assets/Scripts/AppViewMono.cs
+++ Assets/Scripts/AppViewMono.cs
@@ -5,6 +5,9 @@ using TMPro;
 using System;
 using VContainer;
 
+/// <summary>
+/// UIの管理をするクラス
+/// </summary>
 public class AppViewMono : MonoBehaviour
 {
     [Header("フロアレベル")]
@@ -22,18 +25,25 @@ public class AppViewMono : MonoBehaviour
     [Header("移動軌跡")]
     [SerializeField] private Toggle _userTrajectoryToggle; // ユーザーの軌跡を表示するトグル
 
+    [Header("リロード")]
+    [SerializeField] private Button _reloadButton; // リロード
+
     [Header("マップマッチング")]
-    [SerializeField] private Toggle _mapMatchingToggle;
+    [SerializeField] private Toggle _mapMatchingToggle; // マップマッチングのonoffを切り替えるトグル
 
     public event Action<int> OnFloorSelected = null!;
     public event Action OnUserPositionConfirmButtonClicked = null!;
     public event Action OnUserDirectionConfirmButtonClicked = null!;
     public event Action<bool> OnUserTrajectoryToggleChanged = null!;
     public event Action<bool> OnMapMatchingToggleChanged = null!;
+    public event Action OnReloadButtonClicked = null!;
 
     [Inject]
     public void Initialize()
     {
+        _userPositionConfirmButton.interactable = false;
+        _userDirectionConfirmButton.interactable = false;
+
         _floorLevelDropdown.onValueChanged
             .AddListener(selectedIndex => OnFloorSelected?.Invoke(selectedIndex));
 
@@ -46,20 +56,32 @@ public class AppViewMono : MonoBehaviour
         _userTrajectoryToggle.onValueChanged
             .AddListener(isOn => OnUserTrajectoryToggleChanged?.Invoke(isOn));
 
+        _reloadButton.onClick
+            .AddListener(() => OnReloadButtonClicked?.Invoke());
+
         _mapMatchingToggle.onValueChanged
             .AddListener(isOn => OnMapMatchingToggleChanged?.Invoke(isOn));
     }
 
-    // ユーザーの位置をテキストに反映するメソッド
-    void SetUserPositionText(Vector3 userPosition)
+    // ユーザー位置設定への移行用のメソッド
+    public void SwitchToPositionSetup()
     {
-        _userPositionText.text = $"User Position: {userPosition.x:F2}, {userPosition.y:F2}";
+        _userPositionConfirmButton.interactable = true;
     }
 
-    // ユーザーの向きをテキストに反映するメソッド
-    void SetUserDirectionText(float cumulativeYaw)
+    // ユーザー向き設定への移行用のメソッド
+    public void SwitchToDirectionSetup()
     {
-        _userRotationText.text = $"Heading: {cumulativeYaw * Mathf.Rad2Deg:F2}°";
+        _userPositionConfirmButton.interactable = false;
+        _userDirectionConfirmButton.interactable = true;
+    }
+
+    // 初期設定用のUIを非activeにするメソッド
+    public void DeactivateInitialSetupUI()
+    {
+        _floorLevelDropdown.gameObject.SetActive(false);
+        _userPositionConfirmButton.gameObject.SetActive(false);
+        _userDirectionConfirmButton.gameObject.SetActive(false);
     }
 
     // ユーザーのいるフロアをテキストに反映するメソッド
@@ -68,4 +90,15 @@ public class AppViewMono : MonoBehaviour
         _floorLevelText.text = $"Floor Level: {floorLevel}";
     }
 
+    // ユーザーの位置をテキストに反映するメソッド
+    public void SetUserPositionText(Vector3 userPosition)
+    {
+        _userPositionText.text = $"User Position: {userPosition.x:F2}, {userPosition.y:F2}";
+    }
+
+    // ユーザーの向きをテキストに反映するメソッド
+    public void SetUserDirectionText(float cumulativeYaw)
+    {
+        _userRotationText.text = $"Heading: {cumulativeYaw * Mathf.Rad2Deg:F2}°";
+    }
 }
