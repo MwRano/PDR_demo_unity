416a629ce30987bd0d1e2c931a8226cd5421de16
MwRano <noratohira@gmail.com>
2025-06-03 01:52:43 +0900

    Refact MapMatching

--- Assets/Scripts/MapMatching.cs
+++ Assets/Scripts/MapMatching.cs
@@ -1,35 +1,31 @@
 #nullable enable
 using UnityEngine;
-using System.Linq;
+using VContainer;
 
-public class MapMatching : MonoBehaviour
+/// <summary>
+/// マップマッチングを行うクラス
+/// </summary>
+public class MapMatching
 {
-    private RoadSegmentCluster _roadSegmentCluster;
+    private FloorManager _floorManager;
 
-    public MapMatching(RoadSegmentCluster roadSegmentCluster)
+    [Inject]
+    public MapMatching(FloorManager floorManager)
     {
-        _roadSegmentCluster = roadSegmentCluster;
+        _floorManager = floorManager;
     }
 
+    // userを歩行可能なセグメントにマッチングさせるメソッド
     public Vector3 MatchUserToMap(Vector3 position)
     {
-        GameObject closestRoadSegment = _roadSegmentCluster.RoadSegment;
-            // .OrderBy(roadSegment => Vector3.Distance(roadSegment.transform.position, position))
-            // .FirstOrDefault();
+        GameObject roadSegment = _floorManager.CurrentFloorMapData.Value.floorObj;
 
-        Debug.Log($"closest road segment : {closestRoadSegment}");
-        if (closestRoadSegment.TryGetComponent<CompositeCollider2D>(out CompositeCollider2D closestCollider))
+        if (roadSegment.TryGetComponent<CompositeCollider2D>(out CompositeCollider2D collider))
         {
-            return closestCollider.ClosestPoint(position);
-
-            // if (distanceInfo.isValid)
-            // {
-            //     Vector3 closestPoint = distanceInfo.pointA;
-            //     return closestPoint;
-            // }
+            return collider.ClosestPoint(position);
         }
 
-        Debug.LogWarning("不正な処理です");
+        Debug.LogWarning("マップデータに歩行可能なセグメントが設定されていません");
         return new Vector3(0, 0, 0);
     }
 }
